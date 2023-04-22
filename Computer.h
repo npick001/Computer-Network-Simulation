e0225a9decd2a2ed8ad1e2fb5ef3b96372899825
Pepper Whiddon <98910563+ewhidVMASC@users.noreply.github.com>
2023-04-21 23:37:09 -0400

    code cleanup

--- Computer.h
+++ Computer.h
@@ -12,7 +12,6 @@ public:
     std::vector<int> edges;
 
     Computer(Triangular& serviceTimeDist, Exponential& msgGenRateDist, const std::vector<int>& edges);
-
     int GetQueueSize();
     void ReportStatistics();
     int find_next_node(const std::vector<int>& dist, const std::vector<Computer>& nodes);
@@ -22,9 +21,7 @@ private:
     Computer* _connectedEdges;
     FIFO<Message>* _serviceQueue;
     bool _available;
-
     Distribution* _genRate;
-
     class GenerateMessageEA;
     void GenerateMessageEM();
     class ArriveEA;
--- Network.h
+++ Network.h
@@ -10,16 +10,16 @@ public:
 
     std::vector<Computer> nodes;
 
+	int GetEdgeWeight(Computer* computer);
     void ReadFile(std::string filename);
-    int GetEdgeWeight(Computer* computer);
     void addNode(const Computer& node);
     void parseGraphFromFile(const std::string& filename);
 	void equal_weight_dijkstra(int source);
 
 private:
+	std::string read_label(std::istream& in);
 	static Computer* _computerNetwork;
     bool is_valid_node_index(int index, int num_nodes);
-    std::string read_label(std::istream& in);
     void read_edges(std::istream& in, int num_edges, std::vector<int>& edges, int num_nodes, int current_node);
     void read_triangular(std::istream& in, double& min, double& mode, double& max);
     void read_exponential(std::istream& in, double& rate);
