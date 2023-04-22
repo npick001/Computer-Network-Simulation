cdb42b5f79e89c3bc1e82e551f27f76513e933ba
Pepper Whiddon <98910563+ewhidVMASC@users.noreply.github.com>
2023-04-21 23:46:35 -0400

    added main

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,20 @@
 #include "Computer.h"
 #include "Distribution.h"
 #include "EventSet.h"
+#include "Network.h"
 #include "SimulationExecutive.h"
 
 
 int main() {
 	return 0;
 
-	//test github
+	Network myNetwork;
+	myNetwork.ReadFile("foo.txt");
+
+	// Find the shortest path minimizing the number of edges traversed
+	myNetwork.equal_weight_dijkstra(0);
+
+	// Find the shortest path based on the expected cost at each node
+	//myNetwork.dijkstra(0, true);
+
 }
