6dcc0b155026cc44e0bd419c9e27eaec91d13ccf
Nicholas Pickering <npick001@odu.edu>
2023-04-23 14:00:06 -0400

    ea and em

--- Computer.cpp
+++ Computer.cpp
@@ -23,6 +23,98 @@ int Computer::GetQueueSize() {
 void Computer::ReportStatistics()
 {
 }
+class Computer::GenerateMessageEA : public EventAction
+{
+public:
+    GenerateMessageEA(Computer* c) {
+        _c = c;
+    }
+
+    void Execute() {
+        _c->GenerateMessageEM();
+    }
+private:
+    Computer* _c;
+};
+class Computer::ArriveEA : public EventAction
+{
+public:
+    ArriveEA(Computer* c, Message* m) {
+        _c = c;
+        _m = m;
+    }
+
+    void Execute() {
+        _c->ArriveEM(_m);
+    }
+private:
+    Computer* _c;
+    Message* _m;
+};
+class Computer::StartServiceEA : public EventAction
+{
+public:
+    StartServiceEA(Computer* c) {
+        _c = c;
+    }
+
+    void Execute() {
+        _c->StartServiceEM();
+    }
+private:
+    Computer* _c;
+
+};
+class Computer::DoneServiceEA : public EventAction
+{
+public:
+    DoneServiceEA(Computer* c, Message* m) {
+        _c = c;
+        _m = m;
+    }
+
+    void Execute() {
+        _c->DoneServiceEM(_m);
+    }
+private:
+    Computer* _c;
+    Message* _m;
+};
+void Computer::GenerateMessageEM()
+{
+    Time genTime = _genRate->GetRV();
+
+    // send to right place
+    // Create a new message
+    Message* message = new Message(&_computerNetwork->nodes[_id], &_computerNetwork->nodes[finalDestination], genTime);
+
+    // Add the new message to the queue
+    _serviceQueue->AddEntity(message);
+
+    // Schedule the routing event
+    SimulationExecutive::ScheduleEventIn(genTime, new GenerateMessageEA(this));
+};
+void Computer::StartServiceEM() {
+
+    _available = false;
+
+    SimulationExecutive::ScheduleEventIn(_genRate->GetRV(), new DoneServiceEA(this, _serviceQueue->GetEntity()));
+}
+void Computer::ArriveEM(Message* message) {
+
+    _serviceQueue->AddEntity(message);
+    if (_available) {
+        SimulationExecutive::ScheduleEventIn(0.0, new StartServiceEA(this));
+    }
+}
+void Computer::DoneServiceEM(Message* message) {
+
+    _available = true;
+
+    if (_serviceQueue->GetSize() > 0) {
+        SimulationExecutive::ScheduleEventIn(0.0, new StartServiceEA(this));
+    }
+}
 
 void Computer::GenerateMessageEM() {
     Time genTime = _genRate;
