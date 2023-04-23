a7edc09c1afb0ac01712453028e0d2aa0a5a1f4e
Nicholas Pickering <npick001@odu.edu>
2023-04-23 15:05:32 -0400

    Cleaned up main

--- Main.cpp
+++ Main.cpp
@@ -9,16 +9,9 @@ using namespace std;
 void main() {
 	SimulationExecutive::InitializeSimulation();
 
-
-
 	Network network(RoutingAlgorithm::EQUAL_WEIGHT_DIJKSTRA);
 	network.ReadFile("foo.txt");
-	//network.CreateMessage(0, 3);
 	//myNetwork.equal_weight_dijkstra(0);
 
-
-
 	SimulationExecutive::RunSimulation();
-	// Find the shortest path based on the expected cost at each node
-	//myNetwork.dijkstra(0, true);
 }
