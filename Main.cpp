b5583ba1ba0fe3e349aa8c1e956119b39e6e0d9c
Pepper Whiddon <98910563+ewhidVMASC@users.noreply.github.com>
2023-04-25 14:00:33 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -12,7 +12,6 @@ void main() {
 	Time endTime = 1000;
 	InitializeSimulation();
 
-
 	//Network network(RoutingAlgorithm::EQUAL_WEIGHT_DIJKSTRA);
 	Network network(RoutingAlgorithm::WEIGHTED_SHORTEST_PATH);
 	network.SetStats(sh);
